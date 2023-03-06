fa1e29f49b344be2d63d80ccf1a5de9b5f935c46
go-nambu <g.nambu@kakehashi.life>
2023-03-06 16:54:39 +0900

    null-lsのtimeout値調整

--- nvim/init.lua
+++ nvim/init.lua
@@ -77,7 +77,7 @@ require('packer').startup(function(use)
                           group = augroup,
                           buffer = bufnr,
                           callback = function()
-                              vim.lsp.buf.format({bufnr = bufnr})
+                              vim.lsp.buf.format({bufnr = bufnr, timeout_ms = 5000})
                           end,
                       })
                   end
